89021e0c087b920b67c800baf9dd9f2e8529b7fa
PeterL2001 <peterlanger2001@gmail.com>
2021-06-29 21:39:11 +0200

    import one works now

--- heirloomRNG2/heirloomRNG2.js
+++ heirloomRNG2/heirloomRNG2.js
@@ -1,32 +1,7 @@
 
-
-
-//heirloom constants; should make another file for more of those soon
-const Slots = [1, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6];
-const Modifiers = [
-	["Player Efficiency", "Trainer Efficiency", "Storage Size", "Breed Speed", "Trimp Health", "Trimp Attack", "Trimp Block", "Crit Damage", "Crit Chance", "VMDC", "Plaguebringer", "Prismatic Shield", "Gamma Burst"],
-	["Runestone Drop Rate", "Fire Trap Damage", "Strength Tower Effect", "Condenser Effect", "Poison Trap Damage", "Lightning Trap Damage"],
-	["Metal Drop Rate", "Food Drop Rate", "Wood Drop Rate", "Gem Drop Rate", "Fragment Drop Rate", "Farmer Efficiency", "Lumberjack Efficiency", "Miner Efficiency", "Dragimp Efficiency", "Explorer Efficiency", "Scientist Efficiency", "Pet XP"]]
-//list of all possible mods by type
-const Rarities = ["Common", "Uncommon", "Rare", "Epic", "Legendary", "Magnificent", "Ethereal", "Magmatic", "Plagued", "Radiating", "Hazardous"];
-const ModifierUnlocks = [[10, 10, 10, 10, 10, 10, 10, 10, 11, 13, 12], [3, 5, 6, 6, 6, 6, 6], [11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12]];
-const HeirloomValues =
-	//oh god oh fuck this is an 3d Array :(
-	//nvm, future me here, it's a goddamn 4D array
-	//these borders are for type
-	[
-		//Shield
-		[[["0.08", "0.16"], ["0.08", "0.16"], ["0.08", "0.16"], ["0.16", "0.32"], ["0.32", "0.64"], ["0.64", "1.28"], ["1.28", "2.56"], ["2.56", "5.12"], ["5.12", "10.24"], ["10.24", "20.48"], ["20.48", "40.96"]], [["0.1", "0.2"], ["0.1", "0.2"], ["0.1", "0.2"], ["0.2", "0.4"], ["0.4", "0.6"], ["0.6", "0.8"], ["0.8", "1"], ["1", "1.2"], ["1.2", "1.4"], ["", ""], ["", ""]], [["0.32", "0.64"], ["0.32", "0.64"], ["0.32", "0.64"], ["0.64", "1.28"], ["1.28", "2.56"], ["2.56", "5.12"], ["5.12", "7.68"], ["7.68", "10.24"], ["10.24", "12.8"], ["", ""], ["", ""]], [["0.05", "0.1"], ["0.05", "0.1"], ["0.05", "0.1"], ["0.1", "0.2"], ["0.7", "1"], ["1", "1.3"], ["1.3", "1.6"], ["1.6", "1.9"], ["1.9", "2.2"], ["2.2", "2.8"], ["2.6", "3.6"]], [["0.06", "0.2"], ["0.06", "0.2"], ["0.06", "0.2"], ["0.2", "0.4"], ["0.5", "1"], ["1", "1.5"], ["1.5", "2"], ["2", "2.6"], ["2.6", "3.56"], ["3.6", "4.6"], ["6", "7.5"]], [["0.06", "0.2"], ["0.06", "0.2"], ["0.06", "0.2"], ["0.2", "0.4"], ["0.5", "1"], ["1", "1.5"], ["1.5", "2"], ["2", "2.6"], ["2.6", "3.56"], ["3.6", "4.6"], ["6", "7.5"]], [["0.04", "0.07"], ["0.04", "0.07"], ["0.04", "0.07"], ["0.07", "0.1"], ["0.28", "0.4"], ["0.48", "0.6"], ["0.68", "0.8"], ["0.88", "1"], ["1.08", "1.2"], ["", ""], ["", ""]], [["0.4", "0.6"], ["0.4", "0.6"], ["0.4", "0.6"], ["0.6", "1"], ["1", "2"], ["2", "3"], ["3", "4"], ["4", "5"], ["5", "6.5"], ["6.5", "8.5"], ["8.5", "11"]], [["0.014", "0.026"], ["0.014", "0.026"], ["0.014", "0.026"], ["0.026", "0.05"], ["0.05", "0.074"], ["0.074", "0.098"], ["0.098", "0.122"], ["0.123", "0.159"], ["0.2", "0.3"], ["0.3", "0.5"], ["0.5", "0.8"]], [["0.05", "0.07"], ["0.05", "0.07"], ["0.05", "0.07"], ["0.08", "0.11"], ["0.12", "0.16"], ["0.17", "0.22"], ["0.24", "0.3"], ["0.32", "0.38"], ["0.4", "0.5"], ["0.5", "0.6"], ["0.05", "0.07"]], [["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["0.01", "0.15"], ["0.15", "0.3"], ["0.3", "0.45"]], [["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["0.1", "0.5"], ["0.1", "0.4"]], [["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["10", "20"], ["", ""]]],
-		//Core
-		[[["0.1", "0.25"], ["0.1", "0.25"], ["0.1", "0.25"], ["0.25", "0.5"], ["0.5", "1"], ["1", "1.99"], ["2", "4"]], [["0.1", "0.25"], ["0.1", "0.25"], ["0.1", "0.25"], ["0.25", "0.5"], ["0.5", "1"], ["1", "1.99"], ["2", "4"]], [["0.01", "0.1"], ["0.01", "0.1"], ["0.01", "0.1"], ["0.1", "0.2"], ["0.2", "0.5"], ["0.5", "1"], ["1", "1.99"]], [["", ""], ["0.01", "0.05"], ["0.01", "0.05"], ["0.05", "0.1"], ["0.05", "0.15"], ["0.1", "0.2"], ["0.2", "0.3"]], [["", ""], ["0.1", "0.25"], ["0.1", "0.25"], ["0.25", "0.5"], ["0.5", "1"], ["1", "1.99"], ["2", "4"]], [["", ""], ["", ""], ["0.01", "0.1"], ["0.1", "0.2"], ["0.2", "0.5"], ["0.5", "1"], ["1", "1.99"]]],
-		//Staff
-		[[["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["0.03", "0.06"], ["0.03", "0.06"], ["0.03", "0.06"], ["0.06", "0.12"], ["0.16", "0.4"], ["0.32", "0.8"], ["0.64", "1.6"], ["1.28", "3.2"], ["2.56", "6.4"], ["5.12", "12.8"], ["10.24", "25.6"]], [["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["0.25", "0.5"], ["0.5", "1"], ["0.75", "2"]]]
-
-	];
-
-
 //declaring some stuff so it can be used after being initialized in other functions
 var Heirlooms;
+var HeirloomValues;
 
 function processingNeeded() {
 	gNeedProcessing = true;
@@ -47,6 +22,7 @@ function getHeirloomOptions() {
 		game = JSON.parse(gameString);
 	} catch (err) { }
 	var selectorDivision = document.getElementById("selector");
+
 	if (game != null) { selectorDivision.innerHTML = null; }
 
 
@@ -85,47 +61,72 @@ function getHeirloomOptions() {
 	}
 }
 
-function calc(heirloomNumber){
+function calc(heirloomNumber) {
 
 	var Heirloom = Heirlooms[heirloomNumber];
-	var type = typeIndexer(Heirloom.type);
-	var slots = Slots[Heirloom.rarity];
-	var modTypes = [];
-	var fromMax = [];
+	var type = Heirloom.type;
+	var slots = Heirloom.mods.length;
+	var rarity = Heirloom.rarity;
 	var eval = [];
-	for(var i = 0; i < slots; i++){
-		modTypes.push(modIndexer(type,Heirloom.mods[i][0]));
-		fromMax.push(Heirloom.mods[i][3]);
-		var size = HeirloomValues[type][rarity][modTypes[i]];
-		var size = 500;
-		eval.push((size-fromMax[i])/size);
+
+	var resultDivision = document.getElementById("results");
+	resultDivision.innerHTML = null;
+	var totalRating = 0;
+	var totalChange = 1;
+
+	for (var i = 0; i < slots; i++) {
+		var fromMax = Heirloom.mods[i][2];
+		var mod = Heirloom.mods[i][0];
+
+
+		var bigSteppy = HeirloomStats[type][mod]["steps"];
+
+		if (bigSteppy === undefined) {
+			bigSteppy = HeirloomStats.defaultSteps;
+		}
+
+		bigSteppy = bigSteppy[rarity];
+
+		var max = bigSteppy[1];
+		var min = bigSteppy[0];
+
+		var stepSize = bigSteppy[2];
+		var stepCount = (max - min) / stepSize + 1;
+		var rating = fromMax / stepCount;
+
+		var resI = document.createElement('p');
+		resI.innerText = "Drop quality of mod " + mod + ": " + uglyfy(rating);
+		var linebreak = document.createElement('br');
+		resultDivision.appendChild(resI);
+		resultDivision.appendChild(linebreak);
+
+		totalRating += rating;
+		totalChange *= rating;
+
 	}
+	var resR = document.createElement('p');
+	resR.innerText = "Quality of all mods: " + uglyfy(totalRating / slots);
+	var linebreak1 = document.createElement('br');
+	resultDivision.appendChild(resR);
+	resultDivision.appendChild(linebreak1);
 
-
+	var resC = document.createElement('p');
+	resC.innerText = "Chance to an heirloom better in all stats: " + uglyfy(totalChange);
+	var linebreak2 = document.createElement('br');
+	resultDivision.appendChild(resC);
+	resultDivision.appendChild(linebreak2);
 
 
 }
 
-function typeIndexer(type){
-	switch(typeIndexer){
-		case "Shield": return 0;
-		case "Staff": return 2;
-		default: return 1;
-	}
+
+
+
+function uglyfy(floaty) {
+	floaty = (Math.floor(floaty*10000) / 100) + "%";
+	console.log(floaty);
+	return floaty;
 }
-
-function modIndexer(mod,type){
-	var typeMods = Modifiers[type];
-	for(var i = 0; i < typeMods.length; i++)
-	{
-		if(typeMods[i] == mod) return i;
-	}
-}
-
-
-
-
-
 
 
 
@@ -137,6 +138,8 @@ function modIndexer(mod,type){
 
 //random stuff
 function stringify(array) {
+
+
 	out = "[";
 	for (var i = 0; i < array.length; i++) {
 		out += " " + array[i].name;
